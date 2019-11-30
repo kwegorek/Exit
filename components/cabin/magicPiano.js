c6800ef13ac1aaed1429a8f25c5801645f9478b2
kwegorek <ka.wegorek@gmail.com>
2019-11-30 00:00:45 -0500

    adding piano and glasses

--- components/cabin/magicPiano.js
+++ components/cabin/magicPiano.js
@@ -7,14 +7,17 @@ import {
   asset,
   Animated,
   View,
-  VrButton,
+  VrButton,NativeModules 
 } from 'react-360';
 import Entity from 'Entity';
+const { AudioModule } = NativeModules;
 
-
+//regular color 
 let glassObj = '3d_glass_piano/szklanka+2.obj'
 let glassTextureObjRegular = '3d_glass_piano/glass-v2.jpg'
 
+
+//glass color variabels 
 let glassTextureObjDarkYellow = '3d_glass_piano/oie_3022417tXo7WH59.jpg'
 let glassTextureObjBlue = '3d_glass_piano/blue_glass.png'
 let glassTextureObjBlack = '3d_glass_piano/black_glass.png'
@@ -25,6 +28,17 @@ let glassTextureObjLightPink = '3d_glass_piano/light_pink_glass.png'
 let glassTextureObjDarkGrey = '3d_glass_piano/dark_grey.jpg'
 
 
+//music variables
+let noteMidC = '3d_glass_piano/Hear Piano Note - Middle C.wav'
+let noteD = '3d_glass_piano/Hear Piano Note - D.wav'
+let noteE = '3d_glass_piano/Hear Piano Note - Mid E.wav'
+let noteF = '3d_glass_piano/Hear Piano Note - Mid F.wav'
+let noteG = '3d_glass_piano/Hear Piano Note - Mid G.wav'
+let noteA = '3d_glass_piano/Hear Piano Note - Mid A.wav'
+let noteB= '3d_glass_piano/Hear Piano Note - Mid B.wav'
+let noteC = '3d_glass_piano/Hear Piano Note - Middle C.wav'
+
+
 let arrOfTextures = [glassTextureObjDarkGrey,glassTextureObjLightPink,glassTextureObjDarkYellow,glassTextureObjBlue,glassTextureObjGrey, glassTextureObjBlack,glassTextureObjPink,glassTextureObjBrown,]
 
 
@@ -33,12 +47,14 @@ let AnimatedEntity = Animated.createAnimatedComponent(Entity);
 class MagicPiano extends React.Component {
   state = {
     //starting value/initial value for y 
-    clicked: true,
+    clicked: 0,
     yPosition: new Animated.Value(1),
     tableAsset: '3d_piano/O&C_COFFEE TABLE_1200x600x450.obj',
     tableTextureAsset:'3d_piano/O&C_COFFEE TABLE_1200x600x450.mtl 2', 
     glassObj: '3d_glass_piano/szklanka+2.obj',
     glassTextureObj: '3d_glass_piano/glass-v2.jpg',
+    userSequence:[0], 
+    patternSequence:[]
 
   };
 
@@ -48,26 +64,82 @@ class MagicPiano extends React.Component {
 
   playNoteMidC(){
 
+    // let newState = this.state.userSequence.concat([1])
+
+    // this.setState({
+    //     userSequence:newState
+        
+    // })
+    AudioModule.playOneShot({
+        source: asset(noteMidC)
+      });
+
   }
+
+
   playNoteD(){
+
+    AudioModule.playOneShot({
+        source: asset(noteD)
+      });
+
+
       
     }
     playNoteE(){
+
+        AudioModule.playOneShot({
+            source: asset(noteE)
+          });
+
+ 
         
     }
     playNoteF(){
+
+        AudioModule.playOneShot({
+            source: asset(noteF)
+          });
+
+
+      
         
     }
     playNoteG(){
+
+        AudioModule.playOneShot({
+            source: asset(noteG)
+          });
+
+
+    
         
     }
     playNoteA(){
+        AudioModule.playOneShot({
+            source: asset(noteA)
+          });
+
+
+
         
     }
     playNoteB(){
+
+        AudioModule.playOneShot({
+            source: asset(noteB)
+          });
+
+
         
     }
     playNoteC(){
+
+        AudioModule.playOneShot({
+            source: asset(noteC)
+          });
+
+
     }
 
 
@@ -220,7 +292,7 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [86, -200, 190]
+              translate: [87, -200, 190]
             },
             {
               scaleX: 1.0
@@ -262,7 +334,7 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [99, -200, 190]
+              translate: [100, -200, 190]
             },
             {
               scaleX: 1.0
@@ -305,7 +377,7 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [112, -200, 190]
+              translate: [113, -200, 190]
             },
             {
               scaleX: 1.0
@@ -347,7 +419,7 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [125, -200, 190]
+              translate: [128, -200, 190]
             },
             {
               scaleX: 1.0
@@ -389,7 +461,7 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [138, -200, 190]
+              translate: [139, -200, 190]
             },
             {
               scaleX: 1.0
@@ -431,7 +503,7 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [152, -200, 190]
+              translate: [153, -200, 190]
             },
             {
               scaleX: 1.0
