c84baf68e9de1ab5c3b328aac5a1b1bd6cb94302
kwegorek <ka.wegorek@gmail.com>
2019-11-29 21:08:24 -0500

    adding glasses

--- components/cabin/magicPiano.js
+++ components/cabin/magicPiano.js
@@ -13,7 +13,20 @@ import Entity from 'Entity';
 
 
 let glassObj = '3d_glass_piano/szklanka+2.obj'
-let glassTextureObj = '3d_glass_piano/oie_3022417tXo7WH59.jpg'
+let glassTextureObjRegular = '3d_glass_piano/glass-v2.jpg'
+
+let glassTextureObjDarkYellow = '3d_glass_piano/oie_3022417tXo7WH59.jpg'
+let glassTextureObjBlue = '3d_glass_piano/blue_glass.png'
+let glassTextureObjBlack = '3d_glass_piano/black_glass.png'
+let glassTextureObjPink= '3d_glass_piano/pink_glass.png'
+let glassTextureObjBrown = '3d_glass_piano/light_borwn_glass.png'
+let glassTextureObjGrey = '3d_glass_piano/grey_glass.jpg'
+let glassTextureObjLightPink = '3d_glass_piano/light_pink_glass.png'
+let glassTextureObjDarkGrey = '3d_glass_piano/dark_grey.jpg'
+
+
+let arrOfTextures = [glassTextureObjDarkGrey,glassTextureObjLightPink,glassTextureObjDarkYellow,glassTextureObjBlue,glassTextureObjGrey, glassTextureObjBlack,glassTextureObjPink,glassTextureObjBrown,]
+
 
 let AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
@@ -24,8 +37,8 @@ class MagicPiano extends React.Component {
     yPosition: new Animated.Value(1),
     tableAsset: '3d_piano/O&C_COFFEE TABLE_1200x600x450.obj',
     tableTextureAsset:'3d_piano/O&C_COFFEE TABLE_1200x600x450.mtl 2', 
-    glassObj: glassObj, 
-    glassTextureObj: glassTextureObj
+    glassObj: '3d_glass_piano/szklanka+2.obj',
+    glassTextureObj: '3d_glass_piano/glass-v2.jpg',
 
   };
 
@@ -33,12 +46,31 @@ class MagicPiano extends React.Component {
 
   }
 
- showEscapeMap =() => {
+  playNoteMidC(){
+
+  }
+  playNoteD(){
+      
+    }
+    playNoteE(){
+        
+    }
+    playNoteF(){
+        
+    }
+    playNoteG(){
+        
+    }
+    playNoteA(){
+        
+    }
+    playNoteB(){
+        
+    }
+    playNoteC(){
+    }
 
 
-
-
- }
 
 
   render() {
@@ -47,8 +79,6 @@ class MagicPiano extends React.Component {
     return ( <View >
 
       <VrButton>
-
-     {/* round chest next to the table  */}
       <AnimatedEntity
 
       source = {
@@ -61,9 +91,6 @@ class MagicPiano extends React.Component {
       lit = {
         true
       }
-
-
-
     //   texture = {asset(this.state.tableTextureAsset)}
       style = {
         {
@@ -88,12 +115,8 @@ class MagicPiano extends React.Component {
 
       </VrButton>
 
-
-
-
-      <VrButton>
-
-     {/* round chest next to the table  */}
+      {/* C sound */}
+      <VrButton onClick={this.playNoteMidC}>
       <AnimatedEntity
 
       source = {
@@ -113,7 +136,302 @@ class MagicPiano extends React.Component {
       style = {
         {
           transform: [{
-              translate: [60, -200, 150]
+              translate: [60, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+
+     {/* D sound */}
+
+     <VrButton onClick={this.playNoteD}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [73, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+
+      {/* E sound */}
+
+      <VrButton onClick={this.playNoteE}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [86, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+
+      {/* F sound  */}
+
+      <VrButton onClick={this.playNoteF}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [99, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+
+      {/* G sound */}
+
+
+      <VrButton onClick={this.playNoteG}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [112, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+
+      {/* A sound */}
+
+      <VrButton onClick={this.playNoteA}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [125, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+
+      {/* B sound */}
+
+      <VrButton onClick={this.playNoteB}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [138, -200, 190]
+            },
+            {
+              scaleX: 1.0
+            },
+            {
+              scaleY: 1.0
+            },
+            {
+              scaleZ: 1.0
+            },
+          ],
+
+
+        }
+      }
+      />
+
+      </VrButton>
+      
+      {/* C sound */}
+
+      <VrButton onClick={this.playNoteC}>
+      <AnimatedEntity
+
+      source = {
+        {
+          obj: asset(this.state.glassObj),
+        //   mtl: asset(this.state.tableTextureAsset)
+        }
+      }
+
+      lit = {
+        true
+      }
+
+
+
+      texture = {asset(this.state.glassTextureObj)}
+      style = {
+        {
+          transform: [{
+              translate: [152, -200, 190]
             },
             {
               scaleX: 1.0
