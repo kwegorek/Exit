f4d0e02d28a83740d856c369a32e2b121ae31ef5
Andrei Papancea <andreipap10@yahoo.com>
2019-12-02 15:50:22 -0500

    fixed the falling ball

--- components/cabin/fallingPicture.js
+++ components/cabin/fallingPicture.js
@@ -1,87 +1,87 @@
-import React from 'react';
-import { Easing } from 'react-native';
-import {  asset, Animated, View, VrButton, PointLight } from 'react-360';
-import Entity from 'Entity';
+// import React from 'react';
+// import { Easing } from 'react-native';
+// import {  asset, Animated, View, VrButton, PointLight } from 'react-360';
+// import Entity from 'Entity';
 
-const AnimatedEntity = Animated.createAnimatedComponent(Entity);
-
-
-
-class FallingPicture extends React.Component {
-    state = {
-        //starting value/initial value for y
-        yPosition: new Animated.Value(1),
-        textureObj:'picture/album.obj',
-        textureMtl:'picture/album.mtl'
-    };
-    componentDidMount(){
-
-    }
-    startFalling = () => {
-
-        //onmousehover (onEnter - in invoked when cursor is
-        // inside the shape of an object) the y position starts changing
-
-        Animated.timing(
-            this.state.yPosition,
-            {
-              toValue: -2,
-              duration: 5000,
-              delay: 100,
-              easing: Easing.quad
-            }
-            //toValue target value for y, in our case the floor
-            //dealy means every 0.1 sec (100mlsec) the value of yPosition changes
-            //setting easing to easing.bounce means object trajectory beahves like a ball that falls on the ground
-            //easing.quad - realtion is quadratic not linear -> velocity relation to time
-
-          ).start(()=> { this.setState({
-            textureObj:'key/devils-key.obj',
-            textureMtl: 'key/devils-key.mtl'
-        })})
+// const AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
 
 
+// class FallingPicture extends React.Component {
+//     state = {
+//         //starting value/initial value for y
+//         yPosition: new Animated.Value(1),
+//         textureObj:'3d_globe/magic_sphere.obj',
+//         textureMtl:'3d_globe/magic_sphere.mtl'
+//     };
+//     componentDidMount(){
 
-          console.log('state', this.state)
+//     }
+//     startFalling = () => {
+
+//         //onmousehover (onEnter - in invoked when cursor is
+//         // inside the shape of an object) the y position starts changing
+
+//         Animated.timing(
+//             this.state.yPosition,
+//             {
+//               toValue: -2,
+//               duration: 5000,
+//               delay: 100,
+//               easing: Easing.quad
+//             }
+//             //toValue target value for y, in our case the floor
+//             //dealy means every 0.1 sec (100mlsec) the value of yPosition changes
+//             //setting easing to easing.bounce means object trajectory beahves like a ball that falls on the ground
+//             //easing.quad - realtion is quadratic not linear -> velocity relation to time
+
+//           ).start(()=> { this.setState({
+//             textureObj:'3d_mario/mario-sculpture.obj',
+//             textureMtl: '3d_mario/mario-sculpture.mtl'
+//         })})
 
 
 
-          //Animated.timing updates the state until the target calue (toValue) is reached
-    }
+
+//           console.log('state', this.state)
 
 
-    render() {
-        const yPosition = this.state.yPosition
-        console.log(yPosition._value, 'yPosition')
-        console.log(yPosition)
 
-        return (
-
-            <View>
-
-                <AnimatedEntity onEnter={this.startFalling}
-
-                source={{
-                obj: asset(this.state.textureObj),
-                mtl: asset(this.state.textureMtl),
-                }}
-                lit={true}
-                style={{
-                transform: [
-                    { translate: [-10, -6, 1] },
-                    { translateY: yPosition },
-                    { scaleX: 10.01 },
-                    { scaleY: 10.01},
-                    { scaleZ: 10.01 },
-                ],
+//           //Animated.timing updates the state until the target calue (toValue) is reached
+//     }
 
 
-                }}
-                    />
-            </View>
-        )
-    }
-}
+//     render() {
+//         const yPosition = this.state.yPosition
+//         console.log(yPosition._value, 'yPosition')
+//         console.log(yPosition)
 
-export default FallingPicture
+//         return (
+
+//             <View>
+
+//                 <AnimatedEntity onEnter={this.startFalling}
+
+//                 source={{
+//                 obj: asset(this.state.textureObj),
+//                 mtl: asset(this.state.textureMtl),
+//                 }}
+//                 lit={true}
+//                 style={{
+//                 transform: [
+//                     { translate: [-6, -10, 10] },
+//                     { translateY: yPosition },
+//                     { scaleX: 0.08 },
+//                     { scaleY: 0.08},
+//                     { scaleZ: 0.08 },
+//                 ],
+
+
+//                 }}
+//                     />
+//             </View>
+//         )
+//     }
+// }
+
+// export default FallingPicture
