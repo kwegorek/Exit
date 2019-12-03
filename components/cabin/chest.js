2e5ff3e99bd1266c9d1f43184a5cfc47ffd86a6d
Tani314 <tanitabassum299@gmail.com>
2019-12-03 17:30:13 -0500

    document formatted

--- components/cabin/chest.js
+++ components/cabin/chest.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { asset, Animated, View, VrButton, NativeModules } from "react-360";
 import Entity from "Entity";
-import { Easing } from 'react-native';
-import { disableAllExcept } from '../../store/buttons';
-import { connect } from 'react-redux';
+import { Easing } from "react-native";
+import { disableAllExcept } from "../../store/buttons";
+import { connect } from "react-redux";
 
 const AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
@@ -21,34 +21,16 @@ class Chest extends React.Component {
       ]
     },
     clickedOnce: false,
-    zPosition: new Animated.Value(1),
+    zPosition: new Animated.Value(1)
   };
   moveToSkeleton = () => {
-    console.log("trying to move to skeleton")
-    Animated.timing(
-                  this.state.zPosition,
-                  {
-                    toValue: -800,
-                    duration: 3000,
-                    delay: 100,
-                    easing: Easing.quad
-                  }).start(()=> { this.setState({
-                    textureObj: "3d_hat/witch_hat(POSER).obj",
-                    textureObjmtl: "3d_hat/witch_hat.mtl",
-                    styles: {
-                      transform: [
-                        { translate: [200, -500, 700] },
-                        { translateZ: this.state.zPosition },
-                        { scaleX: 700.0 },
-                        { scaleY: 700.0 },
-                        { scaleZ: 700.0 }
-                      ]
-                    }
-                })})
-
-  };
-  openOrclose = () => {
-
+    console.log("trying to move to skeleton");
+    Animated.timing(this.state.zPosition, {
+      toValue: -800,
+      duration: 3000,
+      delay: 100,
+      easing: Easing.quad
+    }).start(() => {
       this.setState({
         textureObj: "3d_hat/witch_hat(POSER).obj",
         textureObjmtl: "3d_hat/witch_hat.mtl",
@@ -56,32 +38,46 @@ class Chest extends React.Component {
           transform: [
             { translate: [200, -500, 700] },
             { translateZ: this.state.zPosition },
-            { rotateY: 200 },
             { scaleX: 700.0 },
             { scaleY: 700.0 },
             { scaleZ: 700.0 }
           ]
         }
       });
-
+    });
+  };
+  openOrclose = () => {
+    this.setState({
+      textureObj: "3d_hat/witch_hat(POSER).obj",
+      textureObjmtl: "3d_hat/witch_hat.mtl",
+      styles: {
+        transform: [
+          { translate: [200, -500, 700] },
+          { translateZ: this.state.zPosition },
+          { rotateY: 200 },
+          { scaleX: 700.0 },
+          { scaleY: 700.0 },
+          { scaleZ: 700.0 }
+        ]
+      }
+    });
   };
 
   handleClick = () => {
- if(!this.state.clickedOnce){
-    this.openOrclose();
-    this.state.clickedOnce = true;
- }
-    else{
+    if (!this.state.clickedOnce) {
+      this.openOrclose();
+      this.state.clickedOnce = true;
+    } else {
       this.moveToSkeleton();
-      this.props.disableButtons('skeletonButton', 'chestButton');}
-
+      this.props.disableButtons("skeletonButton", "chestButton");
+    }
   };
 
   render() {
-    const disableStatus = !this.props.buttons.chestButton
+    const disableStatus = !this.props.buttons.chestButton;
     return (
       <View>
-        <VrButton onClick={this.handleClick} disabled={disableStatus} >
+        <VrButton onClick={this.handleClick} disabled={disableStatus}>
           <AnimatedEntity
             source={{
               obj: asset(this.state.textureObj),
@@ -97,15 +93,18 @@ class Chest extends React.Component {
 }
 const mapStateToProps = state => {
   return {
-    buttons: state.buttons,
+    buttons: state.buttons
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     disableButtons: (buttonToEnable, buttonToDisable) =>
-      dispatch(disableAllExcept(buttonToEnable, buttonToDisable)),
+      dispatch(disableAllExcept(buttonToEnable, buttonToDisable))
   };
 };
 
-export default  connect(mapStateToProps, mapDispatchToProps)(Chest);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Chest);
