628bba1e1f6b1e7b2aa45d017820a8702c9d4641
Tani314 <tanitabassum299@gmail.com>
2019-12-03 17:28:51 -0500

    document formatted

--- components/cabin/bookshelf.js
+++ components/cabin/bookshelf.js
@@ -1,57 +1,37 @@
-import React from 'react';
+import React from "react";
 import {
   asset,
   Animated,
   View,
   VrButton,
   PointLight,
-  NativeModules,
-} from 'react-360';
-import Entity from 'Entity';
+  NativeModules
+} from "react-360";
+import Entity from "Entity";
 const { AudioModule } = NativeModules;
 class Bookshelf extends React.Component {
-  // state = {
-  //     fade: 0.0,
-  //     isFading: true
-  // };
-
-  // componentDidMount(){
-  //     setInterval(() => {
-  //         this.setState((prev) => {
-  //             const isMaxOrMinValue = (prev.fade >= 1.0 || prev.fade <= 0.0)
-  //             const newIsFading = (isMaxOrMinValue ? !prev.isFading : prev.isFading)
-  //             const newFade = prev.fade + (newIsFading ? -0.04 : 0.04)
-  //            // console.log(newFade)
-  //             return {
-  //                 fade: newFade,
-  //                 isFading: newIsFading
-  //             }
-  //         });
-  //     }, 400);
-  //}
   handleClick = () => {
     AudioModule.playOneShot({
-      source: asset('creepy.wav'),
+      source: asset("creepy.wav")
     });
   };
   render() {
-    // const opacityValue = this.state.fade
     return (
       <View>
         <VrButton onClick={this.handleClick}>
           <Entity
             source={{
-              obj: asset('3d_bookshelf/Old_Dusty_Bookshelf.obj'),
-              mtl: asset('3d_bookshelf/Old_Dusty_Bookshelf.mtl'),
+              obj: asset("3d_bookshelf/Old_Dusty_Bookshelf.obj"),
+              mtl: asset("3d_bookshelf/Old_Dusty_Bookshelf.mtl")
             }}
             style={{
               transform: [
                 { translate: [-100, -650, -730] },
                 { scaleX: 3 },
                 { scaleY: 3 },
-                { scaleZ: 3 },
+                { scaleZ: 3 }
               ],
-              color: 'white',
+              color: "white"
             }}
             lit={true}
           ></Entity>
