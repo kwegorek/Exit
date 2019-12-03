949325c9a94af344d76f8a4257cd58498b1504a7
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2019-12-03 15:10:59 -0500

    table restyled

--- components/cabin/Table.js
+++ components/cabin/Table.js
@@ -1,50 +1,38 @@
 import React from 'react';
 import { Easing } from 'react-native';
-import {  asset, Animated, View, VrButton, PointLight } from 'react-360';
+import { asset, Animated, View, VrButton, PointLight } from 'react-360';
 import Entity from 'Entity';
 
 const AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
-
-
 class Table extends React.Component {
-    state = {
+  state = {};
 
-    };
+  componentDidMount() {}
 
-    componentDidMount(){
-
-    }
-
-
-    render() {
-
-
-        return (
-            <View>
-                <VrButton>
-                    <AnimatedEntity
-
-                    source={{
-                    obj: asset('wooden-coffe-table-obj/wooden-coffe-table.obj'),
-                    mtl: asset('wooden-coffe-table-obj/wooden-coffe-table.mtl'),
-                    }}
-                    lit={true}
-                    style={{
-                    transform: [
-                        { translate: [-600, -800, -100] },
-                        { scaleX: 200 },
-                        { scaleY: 200 },
-                        { scaleZ: 200 },
-                    ],
-
-                    }}
-                        />
-
-                </VrButton>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <VrButton>
+          <AnimatedEntity
+            source={{
+              obj: asset('wooden-coffe-table-obj/wooden-coffe-table.obj'),
+              mtl: asset('wooden-coffe-table-obj/wooden-coffe-table.mtl'),
+            }}
+            lit={true}
+            style={{
+              transform: [
+                { translate: [-500, -700, -300] },
+                { scaleX: 200 },
+                { scaleY: 200 },
+                { scaleZ: 200 },
+              ],
+            }}
+          />
+        </VrButton>
+      </View>
+    );
+  }
 }
 
-export default Table
+export default Table;
