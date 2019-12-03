86187fc5817e750956e128835c74cfc2e9f0fa07
Tani314 <tanitabassum299@gmail.com>
2019-12-03 17:43:20 -0500

    document formatted

--- components/cabin/frame.js
+++ components/cabin/frame.js
@@ -1,76 +1,38 @@
-
-import React from 'react';
-import { Easing } from 'react-native';
-import {  asset, Animated, View, VrButton, PointLight } from 'react-360';
-import Entity from 'Entity';
-
-// const AnimatedEntity = Animated.createAnimatedComponent(Entity);
+import React from "react";
+import { Easing } from "react-native";
+import { asset, Animated, View, VrButton, PointLight } from "react-360";
+import Entity from "Entity";
 
 class Frame extends React.Component {
-    state = {
-        // yPosition: new Animated.Value(1),
-        textureObj:'frame/frame.obj',
-        textureMtl:'frame/frame.mtl'
+  state = {
+    textureObj: "frame/frame.obj",
+    textureMtl: "frame/frame.mtl"
+  };
+  componentDidMount() {}
 
-    };
-    componentDidMount(){
-
-    }
-    // startFalling = () => {
-    //     Animated.timing(
-    //         this.state.yPosition,
-    //         {
-    //           toValue: -10,
-    //           duration: 5000,
-    //           delay: 100,
-    //           easing: Easing.quad
-    //         }
-    //       ).start(()=> { this.setState({
-    //         textureObj:'key/devils-key.obj',
-    //         textureMtl:'key/devils-key.mtl'
-    //     })})
-
-
-
-
-        //   console.log('state', this.state)
-
-    // }
-
-
-    render() {
-        // const yPosition = this.state.yPosition
-        // console.log(yPosition._value, 'yPosition')
-        // console.log(yPosition)
-
-        return (
-
-            <View>
-
-                {/* <AnimatedEntity onEnter={this.startFalling} */}
-                <Entity
-                source={{
-                obj: asset(this.state.textureObj),
-                mtl: asset(this.state.textureMtl),
-                }}
-                lit={true}
-                style={{
-                transform: [
-                    { translate: [750, -400,500] },
-                    { rotateX: 270 },
-                    { rotateZ: 50 },
-                    // { translateZ: yPosition },
-                    { scaleX: 10.00 },
-                    { scaleY: 10.00 },
-                    { scaleZ: 10.00},
-                ],
-
-
-                }}
-                    />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Entity
+          source={{
+            obj: asset(this.state.textureObj),
+            mtl: asset(this.state.textureMtl)
+          }}
+          lit={true}
+          style={{
+            transform: [
+              { translate: [750, -400, 500] },
+              { rotateX: 270 },
+              { rotateZ: 50 },
+              { scaleX: 10.0 },
+              { scaleY: 10.0 },
+              { scaleZ: 10.0 }
+            ]
+          }}
+        />
+      </View>
+    );
+  }
 }
 
-export default Frame
+export default Frame;
