daabd96867a83ad93912737e5f641222e1e8be83
Tani314 <tanitabassum299@gmail.com>
2019-12-03 11:59:54 -0500

    formatted

--- components/cabin/book.js
+++ components/cabin/book.js
@@ -1,100 +1,71 @@
-import React from 'react';
-import {
-  asset,
-  Animated,
-  View,
-  VrButton,
-  NativeModules
-} from 'react-360';
-import Entity from 'Entity';
-import { disableAllExcept } from '../../store/buttons';
-import { connect } from 'react-redux';
+import React from "react";
+import { asset, Animated, View, VrButton, NativeModules } from "react-360";
+import Entity from "Entity";
+import { disableAllExcept } from "../../store/buttons";
+import { connect } from "react-redux";
 const { AudioModule } = NativeModules;
 const AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
 let openedBookTexture =
-  'ChurchBookSet/ChurchBookOpenV2/ChurchBookOpenV2-OBJ/Textures/ChurchBookOpenV2-DiffuseHints.png';
+  "ChurchBookSet/ChurchBookOpenV2/ChurchBookOpenV2-OBJ/Textures/ChurchBookOpenV2-DiffuseHints.png";
 
 let closedBooktexture =
-  'ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/Textures/ChurchBookClosedV2-Diffuse.png';
+  "ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/Textures/ChurchBookClosedV2-Diffuse.png";
 
 class Book extends React.Component {
   state = {
-    //starting value/initial value for y
     close: true,
     textureObj:
       "ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.obj",
-     textureObjmtl:
-    "ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.mtl",
-info :'',
-fade: new Animated.Value(0),
-mirrorClue: false,
-mirrorClueSrc: 'Clues/mirrorClue.jpg'
+    textureObjmtl:
+      "ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.mtl",
+    info: "",
+    fade: new Animated.Value(0),
+    mirrorClue: false,
+    mirrorClueSrc: "Clues/mirrorClue.jpg"
   };
   openOrclose = () => {
     if (this.state.close === true) {
       this.setState({
         textureObj:
-          'ChurchBookSet/ChurchBookOpenV2/ChurchBookOpenV2-OBJ/ChurchBookOpenV2.obj',
+          "ChurchBookSet/ChurchBookOpenV2/ChurchBookOpenV2-OBJ/ChurchBookOpenV2.obj",
         textureObjmtl:
-          'ChurchBookSet/ChurchBookOpenV2/ChurchBookOpenV2-OBJ/ChurchBookOpenV2.mtl',
+          "ChurchBookSet/ChurchBookOpenV2/ChurchBookOpenV2-OBJ/ChurchBookOpenV2.mtl",
         close: false,
-        info: 'The riddle for next clue',
+        info: "The riddle for next clue"
       });
     } else {
       this.setState({
         textureObj:
-          'ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.obj',
+          "ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.obj",
         textureObjmtl:
-          'ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.mtl',
+          "ChurchBookSet/ChurchBookClosedV2/ChurchBookClosedV2-OBJ/ChurchBookClosedV2.mtl",
         close: true,
-        info: '',
-        fade: new Animated.Value(0),
+        info: "",
+        fade: new Animated.Value(0)
       });
     }
   };
-  // componentDidUpdate() {
-  //   const { close } = this.state;
-  //   const value = close ? 0 : 1;
-  //   Animated.timing(this.state.fade, {
-  //     toValue: value,
-  //     duration: 3000,
-  //   }).start();
-  // }
 
   handleClick = () => {
-    this.props.disableButtons('faceButton', 'bookButton');
+    this.props.disableButtons("faceButton", "bookButton");
     this.openOrclose();
     AudioModule.playOneShot({
-      source: asset('magic.wav'),
+      source: asset("magic.wav")
     });
     this.setState({
       mirrorClue: true
-    })
+    });
   };
 
   render() {
-    // const { fade } = this.state;
-    const disableStatus = !this.props.buttons.bookButton;//false
+    const disableStatus = !this.props.buttons.bookButton; //false
     return (
       <View>
-        {/* <Animated.Text
-          style={[
-            {
-              color: 'black',
-              fontSize: 6,
-              fontWeight: 'bold',
-              textAlign: 'left',
-            },
-            { opacity: fade },
-          ]}
-        >
-          {this.state.info}
-        </Animated.Text> */}
         <VrButton onClick={this.handleClick} disabled={disableStatus}>
           <AnimatedEntity
             source={{
-              obj: asset(this.state.textureObj),
+              obj: asset(this.state.textureObj)
             }}
             lit={true}
             style={{
@@ -102,46 +73,45 @@ mirrorClueSrc: 'Clues/mirrorClue.jpg'
                 { translate: [-600, -500, -200] },
                 { scaleX: 80.0 },
                 { scaleY: 80.0 },
-                { scaleZ: 80.0 },
-              ],
+                { scaleZ: 80.0 }
+              ]
             }}
-            texture={asset('3d_book/texture/front_side.jpg')}
+            texture={asset("3d_book/texture/front_side.jpg")}
           />
         </VrButton>
-        {this.state.mirrorClue ?
-        <Animated.Image
-        style={{
-          position:'absolute',
-          layoutOrigin: [0.5, 0.5, 0],
-          width: 2,
-          height: 2,
-          transform: [
-
-            {translateZ: -3},
-
-            {translateX:-1.0}
-          ],
-          opacity: 1,
-        }}
-        source={ asset(this.state.mirrorClueSrc)}
-      />: null}
+        {this.state.mirrorClue ? (
+          <Animated.Image
+            style={{
+              position: "absolute",
+              layoutOrigin: [0.5, 0.5, 0],
+              width: 2,
+              height: 2,
+              transform: [{ translateZ: -3 }, { translateX: -1.0 }],
+              opacity: 1
+            }}
+            source={asset(this.state.mirrorClueSrc)}
+          />
+        ) : null}
       </View>
     );
   }
 }
 
 const mapStateToProps = state => {
-  console.log('mapping state to book', state);
+  console.log("mapping state to book", state);
   return {
-    buttons: state.buttons,
+    buttons: state.buttons
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     disableButtons: (buttonToEnable, buttonToDisable) =>
-      dispatch(disableAllExcept(buttonToEnable, buttonToDisable)),
+      dispatch(disableAllExcept(buttonToEnable, buttonToDisable))
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Book);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Book);
