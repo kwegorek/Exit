a247abbbaae94ddc403c076417a061cfb0e5614c
Tani314 <tanitabassum299@gmail.com>
2019-12-03 17:42:18 -0500

    document formatted

--- components/cabin/face.js
+++ components/cabin/face.js
@@ -1,105 +1,104 @@
-import React from 'react';
-import {  asset, Animated, View,  VrButton ,NativeModules} from 'react-360';
-import Entity from 'Entity';
-import { disableAllExcept } from '../../store/buttons';
-import { connect } from 'react-redux';
+import React from "react";
+import { asset, Animated, View, VrButton, NativeModules } from "react-360";
+import Entity from "Entity";
+import { disableAllExcept } from "../../store/buttons";
+import { connect } from "react-redux";
 
 const { AudioModule } = NativeModules;
 
 class Face extends React.Component {
+  state = {
+    fade: 0.0,
+    isFading: true,
+    textFade: new Animated.Value(0),
+    tableClue: false,
+    tableSrc: "Clues/tableClue.jpg"
+  };
 
-    state = {
-        fade: 0.0,
-        isFading: true,
-        textFade: new Animated.Value(0),
-        tableClue: false,
-        tableSrc:"Clues/tableClue.jpg"
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(prev => {
+        const isMaxOrMinValue = prev.fade >= 1.0 || prev.fade <= 0.0;
+        const newIsFading = isMaxOrMinValue ? !prev.isFading : prev.isFading;
+        const newFade = prev.fade + (newIsFading ? -0.04 : 0.04);
+        return {
+          fade: newFade,
+          isFading: newIsFading
+        };
+      });
+    }, 200);
+  }
 
-    };
+  handleClick = () => {
+    this.props.disableButtons("tableButton", "faceButton");
+    AudioModule.playOneShot({
+      source: asset("Laugh.wav")
+    });
 
-    componentDidMount(){
-        setInterval(() => {
-            this.setState((prev) => {
-                const isMaxOrMinValue = (prev.fade >= 1.0 || prev.fade <= 0.0)
-                const newIsFading = (isMaxOrMinValue ? !prev.isFading : prev.isFading)
-                const newFade = prev.fade + (newIsFading ? -0.04 : 0.04)
-                return {
-                    fade: newFade,
-                    isFading: newIsFading
-                }
-            });
-        }, 200);
-    }
-
-    handleClick=()=>{
-        this.props.disableButtons('tableButton', 'faceButton');
-        AudioModule.playOneShot({
-            source: asset('Laugh.wav'),
-          });
-
-          this.setState({
-            tableClue: true
-          })
-    }
-    render() {
-        const opacityValue = this.state.fade
-        const disableStatus = !this.props.buttons.faceButton
-        return (
-
-            <View>
-
-                <VrButton onClick={this.handleClick} disabled={disableStatus}>
-                <Entity source={{
-                obj: asset('face/151out.obj'),
-                }} style={{
-                transform: [
-                    { translate: [750, -270,500] },
-                   { rotateX: 270 },
-                   { rotateZ: 50 },
-                    { scaleX: 30.00 },
-                    { scaleY: 30.00 },
-                    { scaleZ: 30.00},
-                ],
-                color: 'white',
-                opacity: opacityValue
-
-                }}      lit={true}>
-
-                </Entity>
-                </VrButton>
-                {this.state.tableClue ?
-        <Animated.Image
-        style={{
-          position:'absolute',
-          layoutOrigin: [0.5, 0.5, 0],
-          width: 6,
-          height: 6,
-          transform: [
-
-            {translateZ: 5},
-            {translateX: 10},
-            {rotateY: -100}
-          ],
-          opacity: 1,
-        }}
-        source={ asset(this.state.tableSrc)}
-      />: null}
-            </View>
-        )
-    }
+    this.setState({
+      tableClue: true
+    });
+  };
+  render() {
+    const opacityValue = this.state.fade;
+    const disableStatus = !this.props.buttons.faceButton;
+    return (
+      <View>
+        <VrButton onClick={this.handleClick} disabled={disableStatus}>
+          <Entity
+            source={{
+              obj: asset("face/151out.obj")
+            }}
+            style={{
+              transform: [
+                { translate: [750, -270, 500] },
+                { rotateX: 270 },
+                { rotateZ: 50 },
+                { scaleX: 30.0 },
+                { scaleY: 30.0 },
+                { scaleZ: 30.0 }
+              ],
+              color: "white",
+              opacity: opacityValue
+            }}
+            lit={true}
+          ></Entity>
+        </VrButton>
+        {this.state.tableClue ? (
+          <Animated.Image
+            style={{
+              position: "absolute",
+              layoutOrigin: [0.5, 0.5, 0],
+              width: 6,
+              height: 6,
+              transform: [
+                { translateZ: 5 },
+                { translateX: 10 },
+                { rotateY: -100 }
+              ],
+              opacity: 1
+            }}
+            source={asset(this.state.tableSrc)}
+          />
+        ) : null}
+      </View>
+    );
+  }
 }
 const mapStateToProps = state => {
-    return {
-      buttons: state.buttons,
-    };
+  return {
+    buttons: state.buttons
   };
+};
 
-  const mapDispatchToProps = dispatch => {
-    return {
-      disableButtons: (buttonToEnable, buttonToDisable) =>
-        dispatch(disableAllExcept(buttonToEnable, buttonToDisable)),
-    };
+const mapDispatchToProps = dispatch => {
+  return {
+    disableButtons: (buttonToEnable, buttonToDisable) =>
+      dispatch(disableAllExcept(buttonToEnable, buttonToDisable))
   };
+};
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Face)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Face);
--- components/cabin/fallingPicture.js
+++ components/cabin/fallingPicture.js
@@ -1,117 +1,92 @@
-import React from 'react';
-import { Easing } from 'react-native';
-import {  asset, Animated, View, VrButton, PointLight } from 'react-360';
-import { connect } from 'react-redux';
-import { updateCompleted} from '../../store/buttons';
-import Entity from 'Entity';
+import React from "react";
+import { Easing } from "react-native";
+import { asset, Animated, View, VrButton, PointLight } from "react-360";
+import { connect } from "react-redux";
+import { updateCompleted } from "../../store/buttons";
+import Entity from "Entity";
 
 const AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
-
-
 class FallingPicture extends React.Component {
-    state = {
-        //starting value/initial value for y
-        yPosition: new Animated.Value(1),
-        textureObj:'curtain-obj/curtain.obj',
-        textureMtl:'curtain-obj/curtain.mtl', 
-        showEcape:false
-
-    };
-    componentDidMount(){
-        console.log('falling pic monuted')
-
-
+  state = {
+    //starting value/initial value for y
+    yPosition: new Animated.Value(1),
+    textureObj: "curtain-obj/curtain.obj",
+    textureMtl: "curtain-obj/curtain.mtl",
+    showEcape: false
+  };
+  componentDidMount() {
+    console.log("falling pic monuted");
+  }
+  startFalling = () => {
+    const ifGameFinished = this.props.allTasksCompleted.allCompleted;
+    //picture falls only when all Tasks are completed
+    if (ifGameFinished) {
+      Animated.timing(this.state.yPosition, {
+        toValue: -750,
+        duration: 500,
+        delay: 100,
+        easing: Easing.quad
+      }).start(() => {
+        this.setState({
+          showEcape: true,
+          //change to escape object
+          textureObj: "3d_mario/mario-sculpture.obj",
+          textureMtl: "3d_mario/mario-sculpture.mtl"
+        });
+      });
     }
-    startFalling = () => {
-        const ifGameFinished = this.props.allTasksCompleted.allCompleted
-        //picture falls only when all Tasks are completed
-        if(ifGameFinished){
+  };
 
-            Animated.timing(
-                    this.state.yPosition,
-                    {
-                        toValue: -750,
-                        duration: 500,
-                        delay: 100,
-                        easing: Easing.quad
-                    }
-                    ).start(()=> { this.setState({
-
-                    showEcape:true,
-                    //change to escape object
-                    textureObj:'3d_mario/mario-sculpture.obj',
-                    textureMtl: '3d_mario/mario-sculpture.mtl'
-                })})
-
-        }
-
+  showEscape = () => {
+    if (this.state.showEcape) {
+      console.log("Escape Background ------> put the logic here");
     }
+  };
 
-    showEscape = () => {
-
-        if(this.state.showEcape){
-
-            console.log('Escape Background ------> put the logic here')
-
-
-        }
-    }
-
-
-    render() {
-        const yPosition = this.state.yPosition
-        // console.log(yPosition._value, 'yPosition')
-        return (
-
-            <View>
-                <VrButton onClick ={()=> this.showEscape, () => this.startFalling()} >
-                    <AnimatedEntity
-
-                        source={{
-                        obj: asset(this.state.textureObj),
-                        mtl: asset(this.state.textureMtl),
-                        }}
-                        lit={true}
-                        style={{
-                        transform: [
-                            { translate: [-700, -450, 200] },
-                            { translateY: yPosition },
-                            { rotateY: 126},
-                            { rotateX: 0},
-                            { scaleX: 440 },
-                            { scaleY: 500 },
-                            { scaleZ: 440 },
-                        ],
-
-                        }}
-                            />
-                </VrButton>
-
-            </View>
-        )
-    }
+  render() {
+    const yPosition = this.state.yPosition;
+    // console.log(yPosition._value, 'yPosition')
+    return (
+      <View>
+        <VrButton onClick={(() => this.showEscape, () => this.startFalling())}>
+          <AnimatedEntity
+            source={{
+              obj: asset(this.state.textureObj),
+              mtl: asset(this.state.textureMtl)
+            }}
+            lit={true}
+            style={{
+              transform: [
+                { translate: [-700, -450, 200] },
+                { translateY: yPosition },
+                { rotateY: 126 },
+                { rotateX: 0 },
+                { scaleX: 440 },
+                { scaleY: 500 },
+                { scaleZ: 440 }
+              ]
+            }}
+          />
+        </VrButton>
+      </View>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-
-    return {
-
-        allTasksCompleted: state.buttons
-
-    }
-
-  }
+const mapStateToProps = state => {
+  return {
+    allTasksCompleted: state.buttons
+  };
+};
 
 const mapDisaptchToProps = () => {
+  return {
+    updatedCompleted: () => dispatch(updateCompleted())
+  };
+};
 
-    return {
-
-        updatedCompleted: () => dispatch(updateCompleted())
-    }
-}
-
-
-
-
-export default connect(mapStateToProps, mapDisaptchToProps)(FallingPicture);
+export default connect(
+  mapStateToProps,
+  mapDisaptchToProps
+)(FallingPicture);
